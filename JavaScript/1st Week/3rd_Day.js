4a7c4d20706b9f1ab5f2edb9de5171da50356394
RawanAlasmi <rawanal3smi@gmail.com>
2019-08-23 00:16:41 +0300

    3rd Day - Variable Declaration

---
+++ JavaScript/1st Week/3rd_Day.js
@@ -0,0 +1,13 @@
+
+document.write("Let's Start a NEW JOURNEY!!!")
+
+/* In JavaScript there is two style for declaring a variable */
+
+// this is the first style 
+    var a = 3 
+    document.write(a)
+
+// this is the second style 
+    var a 
+    a = 3 
+    document.write(a)
