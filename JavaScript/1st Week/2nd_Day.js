99b05ed071ebf095d96aefd0f046441c6d9ffd9f
RawanAlasmi <rawanal3smi@gmail.com>
2019-08-23 01:51:12 +0300

    --

--- JavaScript/1st Week/2nd_Day.js
+++ JavaScript/1st Week/2nd_Day.js
@@ -1,7 +1,7 @@
 
 document.write("Let's Start a NEW JOURNEY!!!")
 
-// In JavaScript there is two style for writing a comment 
+// In JavaScript there is two types of styles for writing a comment 
 
 // this is the first style 
 /* this is the second style */
