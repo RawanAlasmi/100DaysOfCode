2f4f30baa6ba1830beca7611823a0033d698ae16
RawanAlasmi <rawanal3smi@gmail.com>
2019-08-23 00:55:19 +0300

    4th & 5th days

---
+++ JavaScript/1st Week/4th_and_5th_Days.js
@@ -0,0 +1,5 @@
+/* السؤال:
+اكتب كود تكون مخرجاته أدناه مع ملاحظة أن الرقم 10 هو متغير.
+ */
+
+document.write("Hello, this is my first challenge and I will do it 10 times. <br>I will do it, I will learn JavaScript.")
