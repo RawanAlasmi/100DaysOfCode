387a7d2e3b6bce065ef0ec413a08fb12877c84ec
RawanAlasmi <rawanal3smi@gmail.com>
2019-08-24 09:02:27 +0300

    4th day

--- Python/1st Week/4th_Day.py
+++ Python/1st Week/4th_Day.py
@@ -1,1 +1,26 @@
-""" Python Numbers"""
+""" Python Variables """
+
+""" There are three numeric types in Python: """
+# Int
+x = 1
+
+# Float
+y = 3.5
+
+# Complex
+z = 12E4
+
+""" Type Conversion """
+# from INT to FLOAT
+a = float(x)
+
+# from FLOAT to COMPLEX
+b = complex(y)
+
+# cannot convert complex numbers into another number type.
+
+""" Random Number """
+import random
+print("I ran the example " + str(random.randrange(1, 10)) + " times.")
+
+
