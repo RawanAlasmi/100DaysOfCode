bed5a078013fd6ea1359b8af146d976c858adbc8
RawanAlasmi <rawanal3smi@gmail.com>
2019-09-22 16:24:47 +0300

    week 4

--- JavaScript/4th_Week/25th_and_26th_days.js
+++ JavaScript/4th_Week/25th_and_26th_days.js
@@ -1,4 +1,16 @@
 /* challenge */
 
+str = ""
 
+for (var i = 0; i < 5; i++) {
+loop1: while (i === 1) {
+    continue loop1;
+  }
+  str = str + i;
 
+loop2: while (i === 3) {
+    break loop2;
+ }
+  str = str + i;
+  console.log(str)
+}
